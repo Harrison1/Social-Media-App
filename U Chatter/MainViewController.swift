eceb89b63976b11d163bc1547449b565b5a4a99b
Harrison <hmcguire13@gmail.com>
2014-12-20 14:33:54 -0500

    New Commit

---
+++ U Chatter/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  U Chatter
+//
+//  Created by Harrison McGuire on 12/16/14.
+//  Copyright (c) 2014 harrisonmcguire. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
+
